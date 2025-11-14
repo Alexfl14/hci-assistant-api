b86ef061da05077a476db4db1a90b3301a416505
Florea Alexandru-Florentin <flalex216@gmail.com>
2025-11-14 11:20:24 +0200

    first

---
+++ src/HCI.AIAssistant.API/models/DTOs/AIAssistantController/AIAssistantControllerPostMessageRequestDTO.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCI.AIAssistant.API.models.DTOs.AIAssistantController;
+public class AIAssistantControllerPostMessageRequestDTO
+{
+    public string? TextMessage {get; set;}
+}
